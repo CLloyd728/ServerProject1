d7d01995cbc27089ace7394b70f48315c965f590
Cameron Lloyd <Cam.Lloyd728@gmail.com>
2021-05-06 17:57:37 -0500

    Removed an unecessary file and then changed it over to an express server from the default I was given and added a test response to a put request.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var express = require('express'); // requre the express framework
+var app = express();
 
-http.createServer(function (req, res) {
+// Endpoint to Get a list of users
+app.put('/', (req, res) => {
+    res.end("Test");
+})
+//just some text on the main page to make sure that it is all loading
+app.get('/', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+    res.end('This is the test webpage just to show that it is running\n');
+});
+// Create a server to listen at port 1337
+var server = app.listen(1337, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API demo app listening at http://%s:%s", host, port)
+})
